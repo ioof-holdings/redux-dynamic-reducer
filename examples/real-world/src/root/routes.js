ed834cffd53182985547b84fa677bcfe909529d1
Michael Peyper <michael.peyper@ioof.com.au>
2017-10-11 09:47:18 +1100

    Improved example to only load route component when route is visited

--- examples/real-world/src/root/routes.js
+++ examples/real-world/src/root/routes.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Route } from 'react-router'
-import LoadableRoute from '../common/components/LoadableRoute'
+import Loadable from 'react-loadable'
+
+const Loading = () => <p>Loading...</p>
+
+const loadRoute = (getPromise) => {
+  const RouteComponent = Loadable({
+    loader: () => getPromise(),
+    loading: Loading,
+    render(loaded, props) {
+      const Component = loaded.default;
+      return <Component {...props} />;
+    }
+  })
+  
+  const LoadableRoute = (routeProps) => <RouteComponent {...routeProps} />
+  return LoadableRoute
+}
 
 export default (
-  <Route path="/" component={LoadableRoute(import('../app'))}>
-    <Route path="/:login/:name" component={LoadableRoute(import('../repoPage'))} />
-    <Route path="/:login" component={LoadableRoute(import('../userPage'))} />
+  <Route path="/" component={loadRoute(() => import('../app'))}>
+    <Route path="/:login/:name" component={loadRoute(() =>import('../repoPage'))} />
+    <Route path="/:login" component={loadRoute(() => import('../userPage'))} />
   </Route>
 )
